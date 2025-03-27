c94e4bec4f7875ec34c1de3ffb5abe9642910c49
eduk5r118 <eduardocoussirat@gmail.com>
2025-03-27 08:31:46 -0300

    aula 4

---
+++ aula04/a.ts
@@ -0,0 +1,17 @@
+//1. Introduçãom: por que Typesript?
+
+//JavaScript (sem tipagem):
+function somar(a, b){
+    return a + b;
+}
+console.log(somar(5, "10")); //Resultado inesperado "51"
+
+//*Em JavaScript , você pode passar qualquer tipo de dadp
+
+//* TypeScript (com tipagem):
+function SomarComTipo(a: number, b: number): number {
+    return a + b;
+}
+console.log(SomarComTipo(5, 10));
+somar("oi", "tudo bem?");
+SomarComTipo()
